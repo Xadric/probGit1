81541a4283ef92962e281833171da93a3f9576e6
Xadric <artem.prime2008@gmail.com>
2022-01-12 18:34:50 +0200

    -2-

--- src/com/dtu/Main.java
+++ src/com/dtu/Main.java
@@ -1,8 +1,11 @@
 package com.dtu;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("vyyyuiuiihyhyivhybdfh"+5);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt()+scanner.nextInt());
     }
 }
